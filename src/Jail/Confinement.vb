cadd92d8a0f0ae49dbd22fb15e09970ad651b53d
Barret <format.e.drive@gmail.com>
2017-11-07 16:50:32 +0100

    Add files via upload

--- src/Jail/Confinement.vb
+++ src/Jail/Confinement.vb
@@ -11,7 +11,7 @@ Public NotInheritable Class Confinement
         MyBase.New(Name)
     End Sub
     Protected Friend Function Create(Collection As ICollection, Callback As Action(Of Object, Boolean, Exception)) As ILoader Implements IConfinement.Create
-        Me.Domain = Me.CreateDomain(GetType(Jail).Assembly.Evidence.GetHostEvidence(Of StrongName)())
+        Me.Domain = Me.CreateDomain(Confinement.GetName(Of Jail))
         Me.Loader = Me.CreateLoader(Me.Domain)
         Me.Loader.Append(New Loaders.ByName(GetType(Jail).Assembly.GetName))
 
@@ -43,10 +43,13 @@ Public NotInheritable Class Confinement
         Next
         Return Me.Loader
     End Function
+    Public Shared Function GetName(Of T)() As StrongName
+        Return GetType(T).Assembly.Evidence.GetHostEvidence(Of StrongName)()
+    End Function
     Protected Friend Function CreateLoader(Domain As AppDomain) As Loader
         Return CType(Activator.CreateInstanceFrom(Domain, GetType(Loader).Assembly.CodeBase, GetType(Loader).FullName).Unwrap, Loader)
     End Function
     Protected Friend Function CreateDomain(Name As StrongName) As AppDomain
-        Return AppDomain.CreateDomain(Me.Name, Nothing, Me.Setup, Me.Permissions, Name, GetType(Object).Assembly.Evidence.GetHostEvidence(Of StrongName))
+        Return AppDomain.CreateDomain(Me.Name, Nothing, Me.Setup, Me.Permissions, Name, Confinement.GetName(Of Object))
     End Function
 End Class
